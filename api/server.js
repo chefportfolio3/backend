5ff4ea9a6dbdfaa5cd7d8a830419c6c340e85cff
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-01-07 14:36:34 -0800

    endpoint work without auth

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
+
+const authRouter = require('../auth/auth-router.js');
+// const userRouter = require('../users/users-router.js');
+const recipesRouter = require('../recipes/recipe-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.status(200).json({ api: 'running', environment: process.env.DB_ENV });
+});
+
+server.use('/api/auth', authRouter);
+// server.use('/api/users', userRouter);
+server.use('/api/recipes', recipesRouter);
+
+module.exports = server;
