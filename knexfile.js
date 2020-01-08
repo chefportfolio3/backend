6471d1473a66a8f7ae59429e910ca6df7ad54712
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-01-08 11:13:33 -0800

    deploy test

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,13 @@
 // Update with your config settings.
+require('dotenv').config();
+
+const localPg = {
+	host: 'localhost',
+	database: process.env.DB_NAME,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
 	development: {
@@ -32,18 +41,18 @@ module.exports = {
 	},
 
 	production: {
-		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password'
-		},
+		client: 'pg',
+		connection: dbConnection,
 		pool: {
 			min: 2,
 			max: 10
 		},
 		migrations: {
-			tableName: 'knex_migrations'
+			tableName: 'knex_migrations',
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
 		}
 	}
 };
