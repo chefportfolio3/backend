c3ca91617d9a8d8d53404c5af0ca119d47172630
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-01-08 10:55:27 -0800

    run test

---
+++ api/server.spec.js
@@ -0,0 +1,27 @@
+const server = require('./server');
+const request = require('supertest'); // npm i supertest -D
+require('dotenv').config();
+
+describe('server', () => {
+	it('dv env set to server', () => {
+		expect(process.env.DB_ENV).toBe('testing');
+	});
+
+	describe('GET /', () => {
+		it('should return 200', () => {
+			return request(server)
+				.get('/')
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+		});
+
+		it("should return an 'api' property with the value 'running' inside the body", function() {
+			return request(server)
+				.get('/')
+				.then(res => {
+					expect(res.body.api).toBe('running');
+				});
+		});
+	});
+});
